fc01403e169220d3a7a42195d55a227298ea572e
mcrakhman <rakhmanov.m@gmail.com>
2024-09-02 21:13:16 +0200

    Remove limiter

--- cmd/any-sync-node.go
+++ cmd/any-sync-node.go
@@ -20,7 +20,6 @@ import (
 	"github.com/anyproto/any-sync/net/peerservice"
 	"github.com/anyproto/any-sync/net/pool"
 	"github.com/anyproto/any-sync/net/rpc/debugserver"
-	"github.com/anyproto/any-sync/net/rpc/limiter"
 	"github.com/anyproto/any-sync/net/rpc/server"
 	"github.com/anyproto/any-sync/net/streampool"
 	"github.com/anyproto/any-sync/net/transport/quic"
@@ -131,7 +130,6 @@ func Bootstrap(a *app.App) {
 		Register(server.New()).
 		Register(peerservice.New()).
 		Register(pool.New()).
-		Register(limiter.New()).
 		Register(nodeclient.New()).
 		Register(consensusclient.New()).
 		Register(nodespace.NewStreamOpener()).
