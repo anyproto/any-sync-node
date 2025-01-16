290086c8d85850a5ad093d2021faabc4b27e4e29
mcrakhman <rakhmanov.m@gmail.com>
2025-01-17 00:32:45 +0100

    Add migration check

--- nodespace/migrator/migrator.go
+++ nodespace/migrator/migrator.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
+	"os"
+	"path"
 	"time"
 
+	anystore "github.com/anyproto/any-store"
+	"github.com/anyproto/any-store/anyenc"
 	"github.com/anyproto/any-sync/app"
 	"github.com/anyproto/any-sync/app/logger"
 	"github.com/anyproto/any-sync/commonspace/spacestorage"
@@ -20,6 +23,16 @@ import (
 const CName = "node.nodespace.migrator"
 
 var log = logger.NewNamed(CName)
+
+const (
+	migratedDbFolder = ".migrated"
+	spaceColl        = "space"
+	spaceIdKey       = "spaceId"
+	migratedColl     = "migration"
+	migratedTimeKey  = "time"
+	migratedDoc      = "state"
+	statusKey        = "status"
+)
 
 type noOpProgress struct{}
 
@@ -54,35 +67,115 @@ func (m *migrator) Name() (name string) {
 }
 
 func (m *migrator) Run(ctx context.Context) (err error) {
+	dirPath := path.Join(m.oldPath, migratedDbFolder)
+	err = os.MkdirAll(dirPath, 0755)
+	if err != nil {
+		return err
+	}
+	dbPath := path.Join(dirPath, "store.db")
+	migrateDb, err := anystore.Open(ctx, dbPath, nil)
+	if err != nil {
+		return err
+	}
+	defer migrateDb.Close()
+	if m.checkMigrated(ctx, migrateDb) {
+		return nil
+	}
 	migrator := migration.NewSpaceMigrator(m.oldStorage, m.newStorage, 40, m.path)
 	allIds, err := m.oldStorage.AllSpaceIds()
 	if err != nil {
 		return err
 	}
 	for idx, id := range allIds {
-		if strings.Contains(id, "deletion") {
-			continue
-		}
 		tm := time.Now()
 		err := migrator.MigrateId(ctx, id, noOpProgress{})
-		fmt.Println("[y]: migrated space", id, fmt.Sprintf("%d/%d", idx, len(allIds)), time.Since(tm))
 		if err != nil {
 			if errors.Is(err, migration.ErrAlreadyMigrated) || errors.Is(err, spacestorage.ErrSpaceStorageMissing) {
 				continue
 			}
+			err := m.setSpaceMigrated(ctx, id, migrateDb, false)
+			if err != nil {
+				return err
+			}
 			log.Error("failed to migrate space", zap.String("spaceId", id), zap.Error(err))
+			continue
+		}
+		err = m.setSpaceMigrated(ctx, id, migrateDb, true)
+		if err != nil {
 			return err
 		}
-		log.Info("migrated space", zap.String("spaceId", id), zap.String("total", fmt.Sprintf("%d/%d", idx, len(allIds))))
+		log.Info("migrated space", zap.String("spaceId", id), zap.String("total", fmt.Sprintf("%d/%d", idx, len(allIds))), zap.String("time", time.Since(tm).String()))
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		default:
 		}
 	}
-	return nil
+	return m.setAllMigrated(ctx, migrateDb)
 }
 
 func (m *migrator) Close(ctx context.Context) (err error) {
 	return nil
+}
+
+func (m *migrator) checkMigrated(ctx context.Context, anyStore anystore.DB) bool {
+	coll, err := anyStore.OpenCollection(ctx, migratedColl)
+	if err != nil {
+		return false
+	}
+	_, err = coll.FindId(ctx, migratedDoc)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func (m *migrator) setSpaceMigrated(ctx context.Context, id string, anyStore anystore.DB, isSuccess bool) error {
+	coll, err := anyStore.Collection(ctx, spaceColl)
+	if err != nil {
+		return fmt.Errorf("migration: failed to get collection: %w", err)
+	}
+	arena := &anyenc.Arena{}
+	tx, err := coll.WriteTx(ctx)
+	if err != nil {
+		return err
+	}
+	newVal := arena.NewObject()
+	newVal.Set("id", arena.NewString(id))
+	if isSuccess {
+		newVal.Set(statusKey, arena.NewTrue())
+	} else {
+		newVal.Set(statusKey, arena.NewFalse())
+	}
+	err = coll.Insert(tx.Context(), newVal)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+func (m *migrator) setAllMigrated(ctx context.Context, anyStore anystore.DB) error {
+	coll, err := anyStore.Collection(ctx, migratedColl)
+	if err != nil {
+		return fmt.Errorf("migration: failed to get collection: %w", err)
+	}
+	arena := &anyenc.Arena{}
+	tx, err := coll.WriteTx(ctx)
+	if err != nil {
+		return err
+	}
+	newVal := arena.NewObject()
+	newVal.Set(migratedTimeKey, arena.NewNumberFloat64(float64(time.Now().Unix())))
+	newVal.Set("id", arena.NewString(migratedDoc))
+	err = coll.Insert(tx.Context(), newVal)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return nil
+	}
+	return anyStore.Checkpoint(ctx, true)
 }
